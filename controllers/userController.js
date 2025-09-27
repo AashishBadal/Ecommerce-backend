4fefd5c1c98c0aefa6cc3c755f5e4d86a316c307
AashishBadal <aashishbadal4@gmail.com>
2025-09-27 23:19:14 +0545

    created user and product model

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+
+const loginUser = async (req,res) =>{
+    
+}
+
+const registerUser = async (req,res) =>{
+
+}
+
+const adminLogin = async (req,res) =>{
+
+}
+
+export {loginUser,registerUser,adminLogin}
